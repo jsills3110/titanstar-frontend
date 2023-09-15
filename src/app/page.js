2b2395672f4249eb6b4f27fbd6639cb8c13e31f1
Sills, Jessica <jessica.sills@wizards.com>
2023-09-15 12:08:26 -0700

    Removed tailwindcss from packages. Removed tailwindcss config file. Removed boilerplate code from page.js. Updated title and description in layout.js. Added assets folder with sprites and background.

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,12 @@
 import Image from 'next/image'
+import Link from 'next/Link'
 
 export default function Home() {
-  return <h1>Home</h1>
+  return (
+    <>
+      <header>
+        <Link href="/">TitanStar Legends</Link>
+      </header>
+    </>
+  )
 }
